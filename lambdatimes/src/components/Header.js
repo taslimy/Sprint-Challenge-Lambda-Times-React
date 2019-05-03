909a0d0cfa9cc0bbb478ddfd6d33d1824e495d97
taslimy <taslimer@gmail.com>
2019-05-03 12:57:25 -0400

    Added moment.js to display todays current date.

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment'
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
@@ -6,7 +7,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+      <span className="date">{moment().format('LL')}</span>
       <h1>Lambda Times</h1>
       <span className="temp">98°</span>
     </div>
