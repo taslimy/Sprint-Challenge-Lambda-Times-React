d5979dae0d1ecfc750a93d5a3d75d57a9332eb0e
taslimy <taslimer@gmail.com>
2019-05-03 12:51:47 -0400

    Forgot to change filterCards to filter() fixed my incorrect use of syntax. Tabs are now filtering the topics

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -60,7 +60,7 @@ export default class Content extends Component {
           selectedTab={this.state.selected}
           selectTabHandler={this.changeSelected}
         />
-        <Cards cards={ !this.state.selected === 'all' ? this.filterCards(card => card.tab === this.state.selected) : this.state.cards } />
+        <Cards cards={ this.state.selected !== 'all' ? this.state.cards.filter(card => card.tab === this.state.selected) : this.state.cards } />
       </div>
     );
   }
